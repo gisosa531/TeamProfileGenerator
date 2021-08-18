35d4ff68e476ca262446afddbf6662d6b9b84bdf
gisosa <gio53196@gmail.com>
2021-08-17 18:20:20 -0700

    Directory structure added to be worked on

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+
+const generateTeam = require("");
+
+function buildManagerProfile() {
+    inquirer.prompt([])
+}
