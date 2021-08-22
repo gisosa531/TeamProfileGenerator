4b9f759358dab23f4df074451e5d3236bcd7e133
gisosa <gio53196@gmail.com>
2021-08-22 15:51:30 -0700

    Added requested team classes in lib directory with comments

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+//Packages
+// acquires file system 
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
-const generateTeam = require("");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const renderPage = require("./src/page.js");
+
+const 
 
 function buildManagerProfile() {
-    inquirer.prompt([])
-}
+    console.log("Time to build your team")
+    inquirer.prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the name of the team manager?",
+        },
+        {
+          type: "input",
+          name: "managerIdentificationNumber",
+          message: "What is the id of the manager?",
+        },
+        {
+          type: "input",
+          name: "managerEmailAddress",
+          message: "What is the email address of the manager?",
+        },
+        {
+          type: "input",
+          name: "managerOfficeNumber",
+          message: "What is the manager's office number?",
+        }
+      ]).then(res => {
+        const manager = new Manager(res.managerName, res.managerIdentificationNumber, res.managerEmailAddress, res.managerOfficeNumber);
+       
+      });
+    }
+  
