ec9649b5e696e71235472ca051835cfa15ead41a
Giovanni Sosa <gio53196@gmail.com>
2021-10-02 23:27:53 -0700

    Set up Inquirer prompt function for Manager and modified directories

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 //Packages
-// acquires file system 
+// Acquires file system  and inquirer packages
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
+//Accessing subclasses 
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const renderPage = require("./src/page.js");
+//Access Template
+const renderPage = require("./src/js/page.js");
 
-const 
+//Normalizing paths
+const VIEW_DIR = path.resolve(__dirname, "src/view")
+const viewPath = path.join(VIEW_DIR, "profile.html");
+// console.log(VIEW_DIR)
+// console.log(viewPath)
 
-function buildManagerProfile() {
-    console.log("Time to build your team")
-    inquirer.prompt([
-        {
-          type: "input",
-          name: "managerName",
-          message: "What is the name of the team manager?",
-        },
-        {
-          type: "input",
-          name: "managerIdentificationNumber",
-          message: "What is the id of the manager?",
-        },
-        {
-          type: "input",
-          name: "managerEmailAddress",
-          message: "What is the email address of the manager?",
-        },
-        {
-          type: "input",
-          name: "managerOfficeNumber",
-          message: "What is the manager's office number?",
-        }
-      ]).then(res => {
-        const manager = new Manager(res.managerName, res.managerIdentificationNumber, res.managerEmailAddress, res.managerOfficeNumber);
-       
-      });
+//Global Scope 
+const teamArray = [];
+
+function addManagerProfile() {
+  console.log("Time to build your Dream Team!")
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "managerNAME",
+      message: "What is the Name of the team manager?",
+    },
+    {
+      type: "input",
+      name: "managerID",
+      message: "What is the Id of the manager?",
+    },
+    {
+      type: "input",
+      name: "managerEMAIL",
+      message: "What is the Email address of the manager?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the manager's Office Number?",
     }
+  ]).then(res => {
+    const manager = new Manager(res.managerNAME, res.managerID, res.managerEMAIL, res.officeNumber);
+    teamArray.push(manager);
+    console.log(teamArray);
   
+  });
+}
+
+addManagerProfile()
